1978d0850f39908ccfd5515ee2e0333a278a1feb
Tejaswi Dhulipala <admin@stillwareltd.com>
2026-01-06 00:39:29 +0000

    Move landing page to /zeroed subdirectory and add root redirect

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
+  base: '/zeroed/',
   build: {
+    outDir: 'dist/zeroed',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
